fee9f3dcf66c0a9ae337019961011947942b1474
Tyler Herbert <therbert@mail.uoguelph.ca>
2017-02-25 19:33:25 -0500

    dbClass

---
+++ databaseObject.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import MySQLdb
+
+class Database:
+
+	def __init__(self):
+
+		self.db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="",  		  # your password
+                     db="KDD")            # name of the data base
+
+	def getFields(self, fields, tableType):
+		cur = self.db.cursor(MySQLdb.cursors.DictCursor)
+
+		fieldList = fields[0]
+		for field in fields[1:]:
+			fieldList += ","
+			fieldList += field
+
+		if (tableType == "test"):
+			table = "testData"
+		else:
+			table = "allData"
+
+		# Use all the SQL you like
+		cur.execute("SELECT " + fieldList + " FROM " + table)
+
+		result_set = cur.fetchall()
+		self.db.commit()
+
+		return result_set
